8a2cb9bdb82bd5bd7f9c47bb2ab7f3f1eee4d5a2
ihorshto <igorshtogrin2016@gmail.com>
2022-01-12 15:31:28 +0200

    added shop-filters

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,25 @@
 $ (function(){
+
+  $('.star').rateYo({
+    starWidth: "12px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true
+  });
   
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data){
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+  },
+  });
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
@@ -10,6 +30,8 @@ $ (function(){
   var mixer = mixitup('.products__content');
 
   var mixer = mixitup('.design__items');
+
+  
 
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ function browsersync() {
   browserSync.init({
     server: {
       baseDir: 'app/',
-      index: "about-us.html"
+      index: "shop.html"
     }
   });
 }
@@ -52,6 +52,8 @@ function scripts(){
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/mixitup/dist/mixitup.js',
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
